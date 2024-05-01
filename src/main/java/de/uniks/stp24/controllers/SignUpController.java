3278e06e6c21d65e590a7cfa6c243ffe356baa23
Kristian Schwarz <uk084094@student.uni-kassel.de>
2024-05-01 17:42:22 +0200

    feat(Implemented SignUpController, SignUpService): Implemented most of the features of the SignUpSerivce and -Controller (waiting for others to finish their work to fully implement everything).

--- src/main/java/de/uniks/stp24/controllers/SignUpController.java
+++ src/main/java/de/uniks/stp24/controllers/SignUpController.java
@@ -1,26 +1,100 @@
 package de.uniks.stp24.controllers;
 
-import javafx.fxml.FXML;
+import de.uniks.stp24.App;
+import de.uniks.stp24.dto.CreateUserDto;
+import de.uniks.stp24.rest.SignUpService;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import org.fulib.fx.annotation.controller.Controller;
+import org.fulib.fx.annotation.event.OnRender;
+import org.fulib.fx.annotation.param.Param;
+import de.uniks.stp24.rest.UserApiService;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class SignUpController {
-    @FXML
-    Button registerButton;
-    @FXML
-    TextField usernameField;
-    @FXML
-    TextField passwordField;
+    public TextField usernameField;
+    public TextField passwordField;
+    public TextField errorText;
+    public Button registerButton;
+
+    @Param("username")
+    String username;
+    @Param("password")
+    String password;
+
+    @Inject
+    App app;
+    @Inject
+    SignUpService signUpService;
+    @Inject
+    UserApiService userApiService;
+
+    private final BooleanBinding isLoginFieldEmpty =  this.usernameField.textProperty().isEmpty();
+    private final BooleanBinding isPasswordFieldEmpty = this.passwordField.textProperty().isEmpty();
+
 
     public SignUpController() {
-        registerButton.setOnMousePressed(this::register);
     }
 
-    private void register(MouseEvent mouseEvent) {
+    // Gets inputs from login screen to the signup screen fields.
+    @OnRender
+    private void applyInputs() {
+        if (Objects.nonNull(this.username))
+            this.usernameField.setText(this.username);
+        if (Objects.nonNull(this.password))
+            this.passwordField.setText(this.password);
     }
 
+    // Disables register button when input fields are empty or password inputs do not match.
+    @OnRender
+    private void disableRegisterButton() {
+        this.registerButton.setDisable(true);
+        this.registerButton.disableProperty().bind(
+                this.isLoginFieldEmpty.and(this.isPasswordFieldEmpty));
+        // TODO: After the second password field is added, check for the equality of password inputs!
+    }
 
+    // Shows an error message when input fields are empty or password inputs do not match.
+    @OnRender
+    private void showErrorMessage() {
+        this.errorText.textProperty().bind(Bindings.createStringBinding(() -> {
+            if (this.isLoginFieldEmpty.get()) {
+                return "Please enter a username";
+            }
+            if (this.isPasswordFieldEmpty.get()) {
+                return "Please enter a password";
+            }
+            return "";
+        }, this.isLoginFieldEmpty, this.isPasswordFieldEmpty));
+    }
+
+    private void register() {
+        userApiService.signup(new CreateUserDto(this.getUsername(), this.getPassword()))
+                .subscribe(user -> app.show("/login",
+                        Map.of("username", this.getUsername(),
+                                "password", this.getPassword()
+                        )));
+    }
+
+    // Returns user to the login screen.
+    private void goBack() {
+        app.show("/login",
+                Map.of("username", this.getUsername(),
+                        "password", this.getPassword()
+        ));
+    }
+
+    private String getUsername() {
+        return this.usernameField.getText();
+    }
+
+    private String getPassword() {
+        return this.passwordField.getText();
+    }
 }
---
+++ src/main/java/de/uniks/stp24/rest/SignUpService.java
@@ -0,0 +1,32 @@
+package de.uniks.stp24.rest;
+
+import de.uniks.stp24.controllers.SignUpController;
+import de.uniks.stp24.dto.CreateUserDto;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class SignUpService {
+    @Inject
+    SignUpController signUpController;
+    @Inject
+    UserApiService userApiService;
+
+    // Registers a new user account on server if such username does not exist
+    public boolean register(String username, String password) {
+        if (this.checkForAccountExistence(username))
+                return false;
+        this.userApiService.signup(new CreateUserDto(username, password)).subscribe(
+                user -> Map.of(
+                        "username", username,
+                        "password", password
+                ));
+        return true;
+    }
+
+    // Checks for the existence of an account with the same username
+    // TODO: Implement!
+    private boolean checkForAccountExistence(String username) {
+        return true;
+    }
+}

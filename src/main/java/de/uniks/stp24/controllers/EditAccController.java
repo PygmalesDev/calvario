b2c3ad2bca7885910a6bf651b03e0d12b763ae4c
Tara Stein <tarastein@t-online.de>
2024-05-03 21:07:00 +0200

    feat(edit-Account) Structure of EditAccController

---
+++ src/main/java/de/uniks/stp24/controllers/EditAccController.java
@@ -0,0 +1,49 @@
+package de.uniks.stp24.controllers;
+
+import de.uniks.stp24.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import org.fulib.fx.annotation.controller.Controller;
+import org.fulib.fx.annotation.controller.Title;
+import org.fulib.fx.annotation.event.OnRender;
+import org.fulib.fx.annotation.param.Param;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.Objects;
+
+@Title("Edit Account")
+@Controller
+public class EditAccController {
+
+    @FXML
+    TextField passwordInput;
+    @FXML
+    TextField usernameInput;
+
+    @Inject
+    public EditAccController() {
+    }
+
+    public void saveChanges(ActionEvent actionEvent) {
+
+    }
+
+    public void cancelChanges(ActionEvent actionEvent) {
+    }
+
+    public void continueGame(ActionEvent actionEvent) {
+    }
+
+    public void editAcc(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+    }
+
+    public void goBack(ActionEvent actionEvent) {
+    }
+}

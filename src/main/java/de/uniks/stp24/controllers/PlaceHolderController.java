60fef967c182200416046a42b030e8718f8abad5
mtfmtf <m.torres@student.uni-kassel.de>
2024-05-06 23:01:41 +0200

    feat(place holder): add a place holder screen
    sorry, I did an unintended revert before

---
+++ src/main/java/de/uniks/stp24/controllers/PlaceHolderController.java
@@ -0,0 +1,32 @@
+package de.uniks.stp24.controllers;
+
+
+import de.uniks.stp24.App;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import org.fulib.fx.annotation.controller.Controller;
+import org.fulib.fx.annotation.controller.Title;
+import org.fulib.fx.annotation.event.OnKey;
+
+import javax.inject.Inject;
+
+@Title("place holder")
+@Controller
+public class PlaceHolderController {
+
+    @Inject
+    App app;
+
+    @Inject
+    public PlaceHolderController() {
+    }
+
+    @OnKey()
+    public void keyPressed(KeyEvent event) {
+        System.out.println("key pressed");
+    }
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+        System.out.println("mouse clicked");
+    }
+}

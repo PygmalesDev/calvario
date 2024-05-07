62eb81106cad3d888e6c7fb6bb94c207e96b8c51
mtfmtf <m.torres@student.uni-kassel.de>
2024-05-07 09:21:06 +0200

    test(tokenStorage): add basic test

--- src/test/java/de/uniks/stp24/AppTest.java
+++ src/test/java/de/uniks/stp24/AppTest.java
@@ -1,14 +1,22 @@
 package de.uniks.stp24;
 
+import de.uniks.stp24.service.TokenStorage;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.mockito.Spy;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class AppTest extends ApplicationTest {
 
     @Spy
     public final App app = new App();
+    @Inject
+    TokenStorage tokenStorage;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -19,8 +27,15 @@ public class AppTest extends ApplicationTest {
     }
 
     @Test
-    public void v1() {
-        // TODO login, main-menu, ...
+    public void basicTokenStorage() {
+        tokenStorage = new TokenStorage();
+        assertNull(tokenStorage.getToken());
+        assertNull(tokenStorage.getUserId());
+        tokenStorage.setToken("hallo");
+        tokenStorage.setUserId("user");
+        assertFalse(tokenStorage.getToken().isEmpty());
+        assertFalse(tokenStorage.getUserId().isEmpty());
+
     }
 
 }

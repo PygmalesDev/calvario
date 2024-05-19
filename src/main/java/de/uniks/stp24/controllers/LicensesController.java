eb04daa22e572ab87d51f4ce075849f20ce81bd7
Ashkan Kiafard <ashkan.game.dev@gmail.com>
2024-05-19 06:40:18 +0200

    feat(style): adjusted licenses to look like mockup + added captain in licenses

--- src/main/java/de/uniks/stp24/controllers/LicensesController.java
+++ src/main/java/de/uniks/stp24/controllers/LicensesController.java
@@ -2,11 +2,16 @@ package de.uniks.stp24.controllers;
 
 
 import de.uniks.stp24.App;
+import de.uniks.stp24.component.BubbleComponent;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import org.fulib.fx.annotation.controller.Controller;
 import org.fulib.fx.annotation.controller.Resource;
+import org.fulib.fx.annotation.controller.SubComponent;
 import org.fulib.fx.annotation.controller.Title;
+import org.fulib.fx.annotation.event.OnRender;
 
 import javax.inject.Inject;
 import java.util.ResourceBundle;
@@ -17,11 +22,25 @@ public class LicensesController {
 
     @FXML
     Button backToLoginButton;
+    @FXML
+    Pane captainContainer;
     @Inject
     App app;
     @Inject
     @Resource
     ResourceBundle resources;
+
+    @SubComponent
+    @Inject
+    BubbleComponent bubbleComponent;
+
+    @OnRender
+    public void addSpeechBubble() {
+        captainContainer.getChildren().add(bubbleComponent);
+        Platform.runLater(() -> {
+            bubbleComponent.setCaptainText(resources.getString("pirate.licenses.text"));
+        });
+    }
 
     @Inject
     public LicensesController() {

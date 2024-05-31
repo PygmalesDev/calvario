0b336ccbf0c4b362526bc1788449ad5e80104cc7
Imran17192 <116345684+Imran17192@users.noreply.github.com>
2024-05-31 14:16:07 +0200

    feat(styleLLE): - Changed loading emblem - Changed login emblem - Changed Buttons in LobbySettings.fxml - Changed Buttons in LobbyHostSettings.fxml

--- src/main/java/de/uniks/stp24/App.java
+++ src/main/java/de/uniks/stp24/App.java
@@ -8,7 +8,6 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.fulib.fx.FulibFxApp;
-
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import java.awt.*;

29f54e165a59b916c53a99c12a809779a357ed7a
Alex <alex.khr@yahoo.com>
2024-05-31 11:26:03 +0200

    fix(island-overview): Created Controllers

---
+++ src/main/java/de/uniks/stp24/controllers/IslandOverviewSitesController.java
@@ -0,0 +1,32 @@
+package de.uniks.stp24.controllers;
+
+import org.fulib.fx.annotation.event.OnDestroy;
+import org.fulib.fx.annotation.event.OnInit;
+import org.fulib.fx.annotation.event.OnRender;
+
+import javax.inject.Inject;
+
+public class IslandOverviewSitesController extends BasicController {
+
+    @Inject
+    public IslandOverviewSitesController() {
+
+    }
+
+    @OnInit
+    void init() {
+
+    }
+
+    @OnRender
+    void render() {
+
+    }
+
+    @OnDestroy
+    void destroy() {
+
+    }
+
+
+}

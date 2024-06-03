f013a5a5ae0d005d2e1668a8949f6a816ffb5673
Tara Stein <tarastein@t-online.de>
2024-06-03 15:11:51 +0200

    fix(autologin): Autologin in LoadController
    - subscriber for autologin in loadController
    - changed param autologin of loadController to normal attribute
    - app shows load always first

--- src/main/java/de/uniks/stp24/controllers/LoadController.java
+++ src/main/java/de/uniks/stp24/controllers/LoadController.java
@@ -1,13 +1,16 @@
 package de.uniks.stp24.controllers;
 
+import de.uniks.stp24.service.LoginService;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import org.fulib.fx.annotation.controller.Controller;
 import org.fulib.fx.annotation.controller.Title;
 import org.fulib.fx.annotation.event.OnDestroy;
+import org.fulib.fx.annotation.event.OnInit;
 import org.fulib.fx.annotation.event.OnKey;
-import org.fulib.fx.annotation.param.Param;
+import org.fulib.fx.controller.Subscriber;
+
 import javax.inject.Inject;
 
 @Title("Calvario")
@@ -22,12 +25,35 @@ public class LoadController extends BasicController{
     @FXML
     Image deadBirdsLogo;
 
+    @Inject
+    Subscriber subscriber;
+    @Inject
+    LoginService loginService;
 
-    @Param("autologin")
+
     public boolean autologin;
 
     @Inject
     public LoadController() {
+    }
+
+    @OnInit
+    public void init() {
+        // checks if user wants to login automatically: there is a refreshToken if the user selected remember me before
+        final String refreshToken = prefService.getRefreshToken();
+        if (refreshToken == null || System.getenv("DISABLE_AUTO_LOGIN") != null) {
+            // no automatic login possible
+            this.autologin = false;
+        }
+        try {
+            // login with saved refreshToken
+            subscriber.subscribe(loginService.autologin(refreshToken),
+                    result -> this.autologin = true,
+                    error -> {});
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.autologin = false;
+        }
     }
 
     @OnKey
@@ -49,6 +75,7 @@ public class LoadController extends BasicController{
 
     @OnDestroy
     public void destroy(){
+        subscriber.dispose();
         backgroundHBox = null;
         deadBirdsLogo = null;
         calvarioLogoLoad = null;

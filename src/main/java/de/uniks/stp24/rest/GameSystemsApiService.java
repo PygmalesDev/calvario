35619fdf7deeb5ef3cc540b08f1132c81535820e
Hendrik Martin <hendrikmartin3@web.de>
2024-06-05 11:58:04 +0200

    feat(Structures): Added Dto for GameSystems, GameSystemsAPIService and ResourceService

---
+++ src/main/java/de/uniks/stp24/rest/GameSystemsApiService.java
@@ -0,0 +1,26 @@
+package de.uniks.stp24.rest;
+
+import de.uniks.stp24.dto.CreateSystemsDto;
+import de.uniks.stp24.model.Island;
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.Path;
+
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+public interface GameSystemsApiService {
+
+    @GET("games/{game}/systems")
+    Observable<List<Island>> getAllIslands(@Path("game") String gameID);
+
+    @GET("games/{game}/systems/{id}")
+    Observable<Island> getCertainIsland(@Path("game") String gameID, @Path("id") String ownerID);
+
+    @PATCH("games/{game}/systems/{id}")
+    Observable<CreateSystemsDto> updateIsland(@Path("game") String gameID, @Path("id") String ownerID, @Body CreateSystemsDto dto);
+
+}

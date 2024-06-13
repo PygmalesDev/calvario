152784b50b1bdf61fe04aacd5a0b5e4551df4a1d
Hendrik Martin <hendrikmartin3@web.de>
2024-06-13 11:27:46 +0200

    feat(Structures): Added ComponentGridCell for creating GridCells with components

--- src/main/java/de/uniks/stp24/component/menu/SitePropertiesComponent.java
+++ src/main/java/de/uniks/stp24/component/menu/SitePropertiesComponent.java
@@ -9,7 +9,10 @@ import de.uniks.stp24.service.game.IslandsService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -17,15 +20,18 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import org.controlsfx.control.GridCell;
 import org.controlsfx.control.GridView;
+import org.fulib.fx.FulibFxApp;
 import org.fulib.fx.annotation.controller.Component;
 import org.fulib.fx.annotation.controller.SubComponent;
 import org.fulib.fx.annotation.event.OnInit;
 import org.fulib.fx.annotation.event.OnRender;
+import org.fulib.fx.constructs.listview.ReusableItemComponent;
 import org.fulib.fx.controller.Subscriber;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.function.ToLongBiFunction;
@@ -80,6 +86,8 @@ public class SitePropertiesComponent extends AnchorPane {
     public ObservableList<Map<String, Integer>> resources;
     public ObservableList<ResourceComponent> resourceComponents;
 
+    Provider<ResourceComponent> resourceComponentProvider = ()-> new ResourceComponent(true, true, true, false, gameResourceBundle);
+
 
 
     @OnRender
@@ -125,13 +133,82 @@ public class SitePropertiesComponent extends AnchorPane {
             siteCostsGridView.setCellWidth(150);
             for (Map.Entry<String, Integer> entry : result.cost().entrySet()) {
                 Resource resource = new Resource(entry.getKey(), entry.getValue(), 0);
-                Provider<ResourceComponent> resourceComponentProvider = ()-> new ResourceComponent(true, true, true, false, gameResourceBundle);
                 ResourceComponent resourceComponent = resourceComponentProvider.get();
                 resourceComponent.setItem(resource);
                 resourceComponents.add(resourceComponent);
             }
             siteCostsGridView.setItems(resourceComponents);
         });
+    }
+}
+
+class ComponentGridCell<Item, Component extends Parent> extends GridCell<Item> {
+    private final FulibFxApp app;
+    private final Provider<? extends Component> provider;
+    private final Map<String, Object> extraParams; // extra parameters to pass to the component
+
+    private Component component;
+
+    /**
+     * Creates a new component list cell.
+     *
+     * @param app      The FulibFX app
+     * @param provider The provider to create the component
+     */
+    public ComponentGridCell(FulibFxApp app, Provider<? extends Component> provider) {
+        this(app, provider, Map.of());
+    }
+
+    /**
+     * Creates a new component list cell.
+     *
+     * @param app         The FulibFX app
+     * @param provider    The provider to create the component
+     * @param extraParams Extra parameters to pass to the component
+     */
+    public ComponentGridCell(FulibFxApp app, Provider<? extends Component> provider, Map<String, Object> extraParams) {
+        super();
+        setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        this.app = app;
+        this.provider = provider;
+        this.extraParams = extraParams;
+    }
+
+    @Override
+    protected void updateItem(Item item, boolean empty) {
+        super.updateItem(item, empty);
+
+        // Destroy component if the cell is emptied
+        if (empty || item == null) {
+            if (component != null) {
+                app.destroy(component);
+                component = null;
+            }
+            setGraphic(null);
+            return;
+        }
+
+        // Destroy old component if necessary (if it is not reusable)
+        if (component != null && !(component instanceof ReusableItemComponent<?>)) {
+            app.destroy(component);
+            component = null;
+        }
+
+        // Create and render new component if necessary
+        if (component == null) {
+            component = provider.get();
+            // Add item and list to parameters if they are not already present
+            final Map<String, Object> params = new HashMap<>(extraParams);
+            params.putIfAbsent("item", item);
+            params.putIfAbsent("list", getGridView().getItems());
+            setGraphic(app.initAndRender(component, params));
+        }
+
+        // Update component if possible
+        if (component instanceof ReusableItemComponent<?>) {
+            //noinspection unchecked
+            ((ReusableItemComponent<Item>) component).setItem(item);
+        }
     }
 }
 

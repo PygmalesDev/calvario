ed509050ffa9083c5163235220f9a4b5e35a3234
Imran17192 <116345684+Imran17192@users.noreply.github.com>
2024-06-30 12:43:21 +0200

    feat(Market): Added all FX IDs for marketComponent and fxml. And Removed gridview for normal labels and panes cause its easier that way.

--- src/main/java/de/uniks/stp24/component/game/MarketComponent.java
+++ src/main/java/de/uniks/stp24/component/game/MarketComponent.java
@@ -3,7 +3,10 @@ package de.uniks.stp24.component.game;
 import de.uniks.stp24.controllers.InGameController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import org.controlsfx.control.GridView;
 import org.fulib.fx.annotation.controller.Component;
 import org.fulib.fx.annotation.event.OnInit;
 
@@ -13,6 +16,49 @@ import javax.inject.Inject;
 public class MarketComponent extends StackPane {
     @FXML
     Button closeMarketOverviewButton;
+    @FXML
+    Label userCredits;
+    @FXML
+    Label marketFee;
+    @FXML
+    Label numberOfGoods;
+    @FXML
+    Label priceTag;
+    @FXML
+    Button everySeasonButton;
+    @FXML
+    Button sellButton;
+    @FXML
+    Button buyButton;
+    @FXML
+    Button incrementNumberOfGoods;
+    @FXML
+    Button decrementNumberOfGoods;
+    @FXML
+    Label gemmyAlloysImage;
+    @FXML
+    Label rumImage;
+    @FXML
+    Label sparklingGeodeImage;
+    @FXML
+    Label gunPowderImage;
+    @FXML
+    Label coalImage;
+    @FXML
+    Label provisionImage;
+    @FXML
+    Label gemmyAlloysNumber;
+    @FXML
+    Label rumNumber;
+    @FXML
+    Label sparklingGeodeNumber;
+    @FXML
+    Label gunPowderNumber;
+    @FXML
+    Label coalNumber;
+    @FXML
+    Label provisionNumber;
+
 
     private StorageOverviewComponent storageOverviewComponent;
 
@@ -32,4 +78,6 @@ public class MarketComponent extends StackPane {
     public void closeMarketOverview() {
         this.getParent().setVisible(false);
     }
+
+
 }

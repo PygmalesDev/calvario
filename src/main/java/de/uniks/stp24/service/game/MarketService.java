61d31a331471d1200dbdbb3580454effb4642616
Imran17192 <116345684+Imran17192@users.noreply.github.com>
2024-07-02 00:22:11 +0200

    feat(Market): Added MarketService for serverCommunication with structure and TODOS. Still have to implement.

---
+++ src/main/java/de/uniks/stp24/service/game/MarketService.java
@@ -0,0 +1,13 @@
+package de.uniks.stp24.service.game;
+
+public class MarketService {
+
+
+    //TODO get marketFee
+
+    //TODO create seasonal trades
+
+    //TODO pause seasonal trades
+
+    // TODO delete seasonal trades
+}

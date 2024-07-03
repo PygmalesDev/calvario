af62e23a25b5b2d1fbe1e792de14f4d062281c1c
Hendrik Martin <hendrikmartin3@web.de>
2024-07-03 12:59:07 +0200

    feat(aggregates): Created AggregateService and getter for technology cost and time

--- src/main/java/de/uniks/stp24/rest/GameLogicApiService.java
+++ src/main/java/de/uniks/stp24/rest/GameLogicApiService.java
@@ -1,5 +1,6 @@
 package de.uniks.stp24.rest;
 
+import de.uniks.stp24.dto.AggregateResultDto;
 import de.uniks.stp24.dto.ExplainedVariableDTO;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
@@ -13,5 +14,8 @@ public interface GameLogicApiService {
 
     @GET("games/{game}/empires/{empire}/variables/{variable}")
     Observable<ExplainedVariableDTO> getVariablesExplanations(@Path("empire") String empireID, @Path("variable") String variable);
+
+    @GET("games/{game}/empires/{empire}/aggregates/{aggregate}/technologies/{id}")
+    Observable<AggregateResultDto> getTechnologyCostAndTime(@Path("empire") String empireID, @Path("aggregate") String aggregate, @Path("id") String techID);
 
 }

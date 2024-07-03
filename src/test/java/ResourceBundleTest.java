9cc7c425c1aa5ae68db8cfa9439e32c8f978c0f4
Ashkan Kiafard <ashkan.game.dev@gmail.com>
2024-07-03 14:12:25 +0200

    feat(traits): added resource bundles for variables
    - added a test to check if all resource bundles have the same keys in both languages

---
+++ src/test/java/ResourceBundleTest.java
@@ -0,0 +1,44 @@
+import de.uniks.stp24.App;
+import de.uniks.stp24.ControllerTest;
+import de.uniks.stp24.service.ImageCache;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+
+import javax.inject.Inject;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResourceBundleTest {
+
+    @Spy
+    public ResourceBundle enResources = ResourceBundle.getBundle("de/uniks/stp24/lang/main", Locale.ENGLISH);
+    @Spy
+    public ResourceBundle deResources = ResourceBundle.getBundle("de/uniks/stp24/lang/main", Locale.GERMAN);
+    @Spy
+    public ResourceBundle enGameResources = ResourceBundle.getBundle("de/uniks/stp24/lang/game", Locale.ENGLISH);
+    @Spy
+    public ResourceBundle deGameResources = ResourceBundle.getBundle("de/uniks/stp24/lang/game", Locale.GERMAN);
+    @Spy
+    public ResourceBundle enVariablResources = ResourceBundle.getBundle("de/uniks/stp24/lang/variables", Locale.ENGLISH);
+    @Spy
+    public ResourceBundle deVariablResources = ResourceBundle.getBundle("de/uniks/stp24/lang/variables", Locale.GERMAN);
+
+    @Test
+    public void testBothLangsHaveSameKeys() {
+        Set<String> enResourcesKeys = enResources.keySet();
+        Set<String> deResourcesKeys = deResources.keySet();
+        assertEquals(enResourcesKeys, deResourcesKeys);
+
+        Set<String> enGameResourcesKeys = enGameResources.keySet();
+        Set<String> deGameResourcesKeys = deGameResources.keySet();
+        assertEquals(enGameResourcesKeys, deGameResourcesKeys);
+
+        Set<String> enVariablResourcesKeys = enVariablResources.keySet();
+        Set<String> deVariablResourcesKeys = deVariablResources.keySet();
+        assertEquals(enVariablResourcesKeys, deVariablResourcesKeys);
+    }
+}

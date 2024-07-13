48563270d723eb80b3e2eea2ae385f0c3e1f50a2
ElenaSotoSobrino <elena.soto@web.de>
2024-07-13 14:47:24 +0200

    refactor(project): changing style in license screen and add libaries and websites

--- src/main/java/de/uniks/stp24/controllers/LicensesController.java
+++ src/main/java/de/uniks/stp24/controllers/LicensesController.java
@@ -47,7 +47,6 @@ public class LicensesController extends BasicController {
 
     @OnDestroy
     public void destroy(){
-        backgroundAnchorPane.setStyle("-fx-background-image: null");
-        cardBackgroundVBox.setStyle("-fx-background-image: null");
+
     }
 }

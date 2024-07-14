6bbb2451c1e67743739bece3a4979ec6b33b79e3
Imran17192 <116345684+Imran17192@users.noreply.github.com>
2024-07-14 16:48:59 +0200

    feat(Market): Refactored the SeaonComponent from Record to class and added getters and setters for easier implementation.

--- src/main/java/de/uniks/stp24/model/SeasonComponent.java
+++ src/main/java/de/uniks/stp24/model/SeasonComponent.java
@@ -1,9 +1,57 @@
 package de.uniks.stp24.model;
 
-public record SeasonComponent(
-        String transActionTypeText,
-        String resourceType,
-        int resourceAmount,
-        int moneyAmount
-) {
+public class SeasonComponent {
+    String transActionTypeText;
+    String resourceType;
+    int resourceAmount;
+    int moneyAmount;
+    boolean isPlaying;
+
+    public SeasonComponent(String transActionTypeText, String resourceType, int resourceAmount, int moneyAmount, boolean isPlaying) {
+        this.transActionTypeText = transActionTypeText;
+        this.resourceType = resourceType;
+        this.resourceAmount = resourceAmount;
+        this.moneyAmount = moneyAmount;
+        this.isPlaying = isPlaying;
+    }
+
+    public String getTransActionTypeText() {
+        return transActionTypeText;
+    }
+
+    public void setTransActionTypeText(String transActionTypeText) {
+        this.transActionTypeText = transActionTypeText;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public int getResourceAmount() {
+        return resourceAmount;
+    }
+
+    public void setResourceAmount(int resourceAmount) {
+        this.resourceAmount = resourceAmount;
+    }
+
+    public int getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(int moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public void setPlaying(boolean playing) {
+        isPlaying = playing;
+    }
 }

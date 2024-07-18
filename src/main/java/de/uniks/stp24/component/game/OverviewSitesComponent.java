bd3a2a0b3699d2e5ea4713840643b9c3c2a7b0f6
Kristian Schwarz <uk084094@student.uni-kassel.de>
2024-07-18 15:16:05 +0200

    fix(bug fixes):
    - fixed bug with delete warning screen not showing the icons of the structures
    - fixed NullPointerException appearing when clicking on an island icon inside the empire overview

--- src/main/java/de/uniks/stp24/component/game/OverviewSitesComponent.java
+++ src/main/java/de/uniks/stp24/component/game/OverviewSitesComponent.java
@@ -201,7 +201,8 @@ public class OverviewSitesComponent extends AnchorPane {
         inGameController.sitePropertiesComponent.setVisible(false);
         inGameController.buildingPropertiesComponent.setVisible(false);
         inGameController.overviewContainer.setVisible(false);
-        inGameController.selectedIsland.rudderImage.setVisible(false);
+        if (Objects.nonNull(inGameController.selectedIsland.rudderImage))
+            inGameController.selectedIsland.rudderImage.setVisible(false);
         inGameController.selectedIsland.islandIsSelected = false;
 
         if (!inGameController.islandsService.keyCodeFlag) {

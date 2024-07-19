a6bcc07add12a05dbd8407fe2f1e87f324b19e96
ElenaSotoSobrino <elena.soto@web.de>
2024-07-19 02:26:05 +0200

    refactor(pause-menu): add redesign on pause menu and add information text on it for seasonal trades

--- src/main/java/de/uniks/stp24/component/menu/PauseMenuComponent.java
+++ src/main/java/de/uniks/stp24/component/menu/PauseMenuComponent.java
@@ -2,22 +2,26 @@ package de.uniks.stp24.component.menu;
 
 import de.uniks.stp24.App;
 import de.uniks.stp24.controllers.InGameController;
+import de.uniks.stp24.service.ImageCache;
 import de.uniks.stp24.service.InGameService;
 import de.uniks.stp24.service.TokenStorage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import org.fulib.fx.annotation.controller.Component;
 import org.fulib.fx.annotation.controller.Resource;
 import org.fulib.fx.annotation.event.OnKey;
+import org.fulib.fx.annotation.event.OnRender;
 
 import javax.inject.Inject;
 import java.util.ResourceBundle;
 
 @Component(view = "Pause.fxml")
 public class PauseMenuComponent extends AnchorPane {
+    public ImageView marketInfoImage;
     @FXML
     Button resumeButton;
 
@@ -33,6 +37,8 @@ public class PauseMenuComponent extends AnchorPane {
     App app;
     @Inject
     TokenStorage tokenStorage;
+    @Inject
+    ImageCache imageCache;
 
     @Inject
     InGameService inGameService;
@@ -45,6 +51,12 @@ public class PauseMenuComponent extends AnchorPane {
 
     @Inject
     public PauseMenuComponent() {
+
+    }
+
+    @OnRender
+    public void render() {
+        marketInfoImage.setImage(imageCache.get("icons/information-button2.png"));
 
     }
 

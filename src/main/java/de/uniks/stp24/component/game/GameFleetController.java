5c24ec0547b70d7ba0269edd1f48b00e89213321
Kristian Schwarz <uk084094@student.uni-kassel.de>
2024-07-22 21:32:44 +0200

    feat(fleets): - implemented FleetService and FleetApiService for Server communication - implemented FleetCreationComponent as an in-game representation of the fleet - Fleets are now displayed near their corresponding islands in a radius around them - Fleets can be clicked and moved around the map (for testing purposes) - added collision circles for island and Fleets. circle radius can be changed in the constants. circles can be displayed by pressing ctrl+R - added empty boolean methods for islands and Fleets to check for collisions with other objects

---
+++ src/main/java/de/uniks/stp24/component/game/GameFleetController.java
@@ -0,0 +1,32 @@
+package de.uniks.stp24.component.game;
+
+import de.uniks.stp24.service.game.FleetCoordinationService;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import org.fulib.fx.annotation.controller.Component;
+
+import javax.inject.Inject;
+
+@Component(view = "GameFleet.fxml")
+public class GameFleetController extends Pane {
+    public Circle activeCircle;
+    public Circle collisionCircle;
+
+    FleetCoordinationService fleetCoordinationService;
+
+    @Inject
+    public GameFleetController(FleetCoordinationService fleetCoordinationService){
+        this.fleetCoordinationService = fleetCoordinationService;
+    };
+
+    public void setActive() {
+        System.out.println("triggered!");
+        this.activeCircle.setVisible(!this.activeCircle.isVisible());
+        this.fleetCoordinationService.setFleet(this);
+    }
+
+    public boolean isCollided(Circle other) {
+        return this.collisionCircle.intersects(other.getLayoutBounds());
+    }
+
+}

9fd605c382cf8373fe1ebf45695e9c4447636830
Imran17192 <narmi3002@gmail.com>
2024-07-22 23:37:41 +0200

    fix(Market): Component, FXML, Style and Service for wars
    - Created ApiService for wars with impl.
    - Created DTOs for warApiService and warService with impl.
    - Created Fxml for warPopUp with basic style
    - Created styleSheet for wars
    - Created Component file for wars

---
+++ src/main/java/de/uniks/stp24/service/game/WarService.java
@@ -0,0 +1,40 @@
+package de.uniks.stp24.service.game;
+
+import de.uniks.stp24.dto.CreateWarDto;
+import de.uniks.stp24.dto.UpdateWarDto;
+import de.uniks.stp24.dto.WarDto;
+import de.uniks.stp24.rest.WarsApiService;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class WarService {
+    @Inject
+    public  WarsApiService warsApiService;
+
+    @Inject
+    public WarService(){
+
+    }
+
+    public Observable<CreateWarDto> createWar(String gameID, CreateWarDto createWarDto) {
+        return this.warsApiService.createWar(gameID, createWarDto);
+    }
+
+    public Observable<List<WarDto>> getWars(String gameID, String empireID) {
+        return this.warsApiService.getWars(gameID, empireID);
+    }
+
+    public Observable<WarDto> getWar(String gameID, String warID) {
+        return this.warsApiService.getWar(gameID, warID);
+    }
+
+    public Observable<UpdateWarDto> updateWar(String gameID, String warID, UpdateWarDto updateWarDto) {
+        return this.warsApiService.updateWar(gameID, warID, updateWarDto);
+    }
+
+    public Observable<WarDto> deleteWar(String gameID, String warID) {
+        return this.warsApiService.deleteWar(gameID, warID);
+    }
+}

1b28695bba83b36ba407647dcfec51748eca52a2
ElenaSotoSobrino <elena.soto@web.de>
2024-07-25 16:49:12 +0200

    fix(buildings): Add information for shipyard and fortress in produce list

--- src/main/java/de/uniks/stp24/component/game/BuildingPropertiesComponent.java
+++ src/main/java/de/uniks/stp24/component/game/BuildingPropertiesComponent.java
@@ -14,6 +14,7 @@ import de.uniks.stp24.service.game.*;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -55,6 +56,11 @@ public class BuildingPropertiesComponent extends AnchorPane {
     ImageView buildingImage;
     @FXML
     Pane jobProgressPane;
+    @FXML
+    Pane textInfoPane;
+    @FXML
+    Label textInfoLabel;
+
     @Inject
     public JobsService jobsService;
     @Inject
@@ -289,6 +295,19 @@ public class BuildingPropertiesComponent extends AnchorPane {
             Map<String, Integer> resourceMapCost = buildingDto.cost();
             ObservableList<Resource> resourceListCost = resourcesService.generateResourceList(resourceMapCost, buildingCostsListView.getItems(), null, false);
             buildingCostsListView.setItems(resourceListCost);
+        }
+
+        /* Sets infoText for buildings that does not produce anything */
+        if (buildingProducesListView.getItems().isEmpty()) {
+            textInfoPane.setVisible(true);
+            if (buildingDto.id().equals("fortress")) {
+                textInfoLabel.setText(gameResourceBundle.getString("building.stronghold.info"));
+            } else if (buildingDto.id().equals("shipyard")) {
+                textInfoLabel.setText(gameResourceBundle.getString("building.shipyard.info"));
+            }
+        } else {
+            textInfoPane.setVisible(false);
+            textInfoLabel.setText("");
         }
     }
 

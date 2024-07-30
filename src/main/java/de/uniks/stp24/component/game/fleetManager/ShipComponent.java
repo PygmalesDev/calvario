64c8b904837f81229f3836f2692ca2b885bc38e0
Tara Stein <tarastein@t-online.de>
2024-07-30 10:01:14 +0200

    feat(fleet-manager): show information of ship
    - added setItem method in shipComponent
    - smaller changes in fxml

--- src/main/java/de/uniks/stp24/component/game/fleetManager/ShipComponent.java
+++ src/main/java/de/uniks/stp24/component/game/fleetManager/ShipComponent.java
@@ -1,15 +1,22 @@
 package de.uniks.stp24.component.game.fleetManager;
 
+import de.uniks.stp24.model.Ships;
+import de.uniks.stp24.service.TokenStorage;
+import de.uniks.stp24.service.game.ShipService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import org.fulib.fx.annotation.controller.Component;
+import org.fulib.fx.constructs.listview.ReusableItemComponent;
+import org.fulib.fx.controller.Subscriber;
 
 import javax.inject.Inject;
 
+import static de.uniks.stp24.model.Ships.*;
+
 @Component(view = "Ship.fxml")
-public class ShipComponent extends VBox {
+public class ShipComponent extends VBox implements ReusableItemComponent<ReadShipDTO> {
     @FXML
     public Label blueprintTypeLabel;
     @FXML
@@ -29,11 +36,35 @@ public class ShipComponent extends VBox {
     @FXML
     public Label defenseLabel;
 
+    @Inject
+    ShipService shipService;
+    @Inject
+    Subscriber subscriber;
+    @Inject
+    TokenStorage tokenStorage;
+    private ReadShipDTO readShipDTO;
+
 
     @Inject
     public ShipComponent(){}
 
+    @Override
+    public void setItem(ReadShipDTO shipDTO){
+        this.readShipDTO = shipDTO;
+        blueprintTypeLabel.setText(shipDTO.type());
+        healthLabel.setText(String.valueOf(shipDTO.health()));
+        int shipTypeIndex = shipService.shipTypesAttributes.indexOf(shipDTO.type());
+        ShipType shipType = shipService.shipTypesAttributes.get(shipTypeIndex);
+        speedLabel.setText(String.valueOf(shipType.speed()));
+        attackLabel.setText(String.valueOf(shipType.attack().get("default")));
+        defenseLabel.setText(String.valueOf(shipType.defense().get("default")));
+    }
+
     public void changeFleet(){}
 
-    public void deleteShip(){}
+    public void deleteShip(){
+        this.subscriber.subscribe(this.shipService.deleteShip(readShipDTO),
+                result -> {},
+                error -> System.out.println("Error while deleting a ship in the ShipComponent:\n" + error.getMessage()));
+    }
 }
--- src/main/java/de/uniks/stp24/model/Ships.java
+++ src/main/java/de/uniks/stp24/model/Ships.java
@@ -1,5 +1,6 @@
 package de.uniks.stp24.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ships {
@@ -26,6 +27,7 @@ public class Ships {
             String empire,
             String fleet,
             String type,
+            int health,
             int experience,
             Map<String, Object> _public
     ) {}
@@ -52,4 +54,8 @@ public class Ships {
             int count,
             Fleets.Fleet fleet
     ){}
+
+    public static Ships.ReadShipDTO readShipDTOFromShip(Ship dto) {
+        return new Ships.ReadShipDTO(dto.createdAt, dto.updatedAt, dto._id, dto.game, dto.empire, dto.fleet, dto.type, dto.health, dto.experience, dto._public);
+    }
 }

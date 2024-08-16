0dae2eb7107c7295784a91aa02391f1b0d13a951
Ashkan Kiafard <ashkan.game.dev@gmail.com>
2024-08-16 15:36:00 +0200

    feat: added icon and job inspector to ship and travel jobs

--- src/main/java/de/uniks/stp24/controllers/InGameController.java
+++ src/main/java/de/uniks/stp24/controllers/InGameController.java
@@ -750,6 +750,9 @@ public class InGameController extends BasicController {
                 showTechnologies()
         );
 
+        this.jobsService.setJobInspector("show_fleet_manager", (Jobs.Job job) ->
+                showFleetManager()
+        );
     }
 
     public void showOverview() {

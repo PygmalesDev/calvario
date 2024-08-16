ec0f1455a94ed03e4a1caaed31bdc7829b4fa3ca
Tara Stein <tarastein@t-online.de>
2024-08-16 15:45:35 +0200

    fix(war disco2): Fixed test

--- src/test/java/de/uniks/stp24/appTestModules/IngameModule.java
+++ src/test/java/de/uniks/stp24/appTestModules/IngameModule.java
@@ -363,7 +363,7 @@ public class IngameModule extends LobbyTestLoader {
 
         when(this.gameLogicApiService.getAggregate(any(),any(),any())).thenReturn(Observable.just(HEALTH_DEF_DTO));
         when(this.warService.getWars(any(),any())).thenReturn(Observable.just(EMPTY_WARDTO_LIST));
-        when(this.empireApiService.getPrivate(any(),any())).thenReturn(Observable.just(new EmpirePrivateDto(new HashMap<>())));
+        when(this.empireApiService.getPrivate(any(),any())).thenReturn(Observable.just(new EmpirePrivate(new HashMap<>())));
 
     }
 

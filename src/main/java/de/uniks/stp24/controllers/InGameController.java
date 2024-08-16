f554089047230240f66a349562e74868c5f7f57c
Ashkan Kiafard <ashkan.game.dev@gmail.com>
2024-08-16 02:04:08 +0200

    feat/fog-of-war: tried to fix tests

--- src/main/java/de/uniks/stp24/controllers/InGameController.java
+++ src/main/java/de/uniks/stp24/controllers/InGameController.java
@@ -934,7 +934,7 @@ public class InGameController extends BasicController {
     }
 
     public void removeFogFromIsland(boolean animate, IslandComponent island) {
-        if (island.foggy) {
+        if (Objects.nonNull(island) && island.foggy) {
             this.fogOfWar.removeFogFromIsland(island);
             if (animate) this.updateFogAnimated();
             else this.updateFog();
